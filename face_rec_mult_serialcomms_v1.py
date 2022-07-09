5dc14f9bfcf21be49a3a222f7e6fb811618be6cc
fivefishstudios <ruel@fivefish.net>
2022-07-08 20:22:21 -0500

    Face recognition using USB Camera + Serial comms

---
+++ face_rec_mult_serialcomms_v1.py
@@ -0,0 +1,108 @@
+# face_rec_mult_serialcomms_v1.py
+# face recognition using /known and /unknown directory of images
+# feed this an unknown image and will go through the /known faces to match and label the unknown image
+# if face is recognized, send serial comms to ESP32
+# pre-process all known images and save into memory all face encodings
+# 7.3.22
+
+import time
+import cv2
+import face_recognition
+import os
+import pickle5 as pickle
+import sys
+import serial
+
+# setup serial port/USB
+serial_out_port = '/dev/cu.usbserial-1430'
+ser = serial.Serial(serial_out_port, 115200, timeout=0.050)
+
+start_time = time.time()
+# unknown_photo = './unknown_faces/familypic2.jpg'
+# unknown_photo = './unknown_faces/steve-b-skateboard.jpg'
+# unknown_photo = './unknown_faces/rose1.jpg'
+# unknown_photo = './unknown_faces/bradpitt-with-glasses.jpg'
+# unknown_photo = './unknown_faces/erin-play.jpg'
+# unknown_photo = './unknown_faces/leslie-kitt.jpg'
+# unknown_photo = './unknown_faces/gallery1.jpg'
+# unknown_photo = './unknown_faces/erin-nathan.jpg'
+# unknown_photo = './unknown_faces/erin1.jpg'
+# unknown_photo = './unknown_faces/justice-league.jpg'
+# unknown_photo = './unknown_faces/erin-baby.png'
+# unknown_photo = './unknown_faces/familypic2.jpg'
+# unknown_photo = './unknown_faces/gallery1.jpg'
+
+# Load Face Training Data (Face-Training-Data.pkl)
+face_encodings = {}  # Dictionary Object  'face_label' : 99999
+try:
+    with open("Face-Training-Data.pkl", "rb") as file:
+        face_encodings = pickle.load(file)
+        # print(face_encodings)
+        # sys.exit("Stop here...")
+except FileNotFoundError:
+    sys.exit("Error: Face-Training-Data.pkl not found. Run the Training Program first.")
+
+# setup camera for live capture
+cam = cv2.VideoCapture(0)
+
+while cam.isOpened():
+    load_time = time.time()
+    # our unknown photo, may contain single or multiple faces
+    success, unknown_image = cam.read()
+
+    # find faces in unknown image
+    face_locations_unknown_image = face_recognition.face_locations(unknown_image)
+
+    # for each face found in the unknown image, run it through our known images directory to try and identify person
+    for face_location in face_locations_unknown_image:
+        # get location of current face found in unknown image
+        top, right, bottom, left = face_location
+        # extract the face (region of interest)
+        unknown_face_image = unknown_image[top:bottom, left:right]  # height, width
+
+        unknown_face = face_recognition.face_encodings(unknown_face_image)
+        if unknown_face:
+            unknown_face_encoding = face_recognition.face_encodings(unknown_face_image)[0]  # encode unknown face
+        else:
+            break
+
+        # loop through all pre-processed face encodings and compare against the unknown image
+        for face_encoding in face_encodings:
+            # display progress status
+            # print('.', end='')
+            # check if we have a match for this particular face
+            # but first, we need to get actual value from dictionary based on current key (face_encoding)
+            match = face_recognition.compare_faces(unknown_face_encoding, [face_encodings.get(face_encoding)])
+            if match[0]:
+                face_label = face_encoding  # the current dictionary key is the matching person's name
+                break  # move to next face, since we already found a match in encoding
+            else:
+                face_label = "Unknown"
+            if cv2.waitKey(1) == ord('q'):
+                exit(0)
+
+        cv2.rectangle(unknown_image, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.rectangle(unknown_image, (left, top), (right, top - 50), (0, 255, 0), -1)
+        cv2.putText(unknown_image, face_label, (left + 10, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3)
+        # after we go through all the known face encodings for this particular face, process the next face...
+
+        # detect if Jon Snow
+        if face_label == "Jon Snow":
+            print("Jon Snow detected")
+            # send data to ESP32
+            ser.write('{}\n'.format(face_label).encode())
+        else:
+            ser.write('Unknown\n'.encode())
+
+    end_time = time.time()
+    # print("\nLoading Face Data took {:2f} seconds".format(load_time-start_time))
+    print("Face Recognition took {:2f} seconds".format(end_time - load_time))
+    # print("Total processing took {:2f} seconds".format(end_time-start_time))
+
+    # display results!
+    # cv2.imshow('Output', cv2.cvtColor(unknown_image, cv2.COLOR_BGR2RGB))
+    cv2.imshow('Output', unknown_image)
+    if cv2.waitKey(1) == ord('q'):
+        exit(0)
+
+cv2.destroyAllWindows()
